fd8e55c34926c694fdc529a72cd08738a54de31f
Ryanward02 <ryanward02@gmail.com>
2017-06-08 16:04:03 +0100

    Adding website files

---
+++ template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+  
+  <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+  
+  <head>
+    <title>
+	  <!-- Enter title -->
+    </title>
+  </head>
+  
+  <body>
+    <?php include_once("analyticstracking.php") ?>
+	<div id="header">
+	  <a href="index.php">
+        <div class="navbox">Home</div>
+	  </a>
+	  <a href="downloads.php">
+	    <div class="navbox">Downloads</div>
+	  </a>
+	  <a href="recent.php"> 
+	    <div class="navbox">Recent Blogs</div>
+	  </a>
+	  <a href="about.php">
+	    <div class="navbox">About</div>
+	  </a>
+	  <a href="help.php">
+	    <div class="navbox">Help / Contact</div>
+	  </a>
+	</div>
+	
+</html>
