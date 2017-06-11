9ac8fd6f36165c74372a701a2c27f7e6a0398ab4
Ryanward02 <ryanward02@gmail.com>
2017-06-11 21:38:50 +0100

    Website files

---
+++ help.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+  
+  <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+  
+  <head>
+    <title>
+	  Help Page
+    </title>
+	<script>
+	
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-100432608-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  </head>
+  
+  <body>
+    <?php include_once("analyticstracking.php") ?>
+	<div id="header">
+	  <a href="index.php">
+        <div class="navbox">Home</div>
+	  </a>
+	  <a href="downloads.php">
+	    <div class="navbox">Downloads</div>
+	  </a>
+	  <a href="recent.php"> 
+	    <div class="navbox">Recent Blogs</div>
+	  </a>
+	  <a href="about.php">
+	    <div class="navbox">About</div>
+	  </a>
+	  <a href="help.php">
+	    <div class="navbox">Help / Contact</div>
+	  </a>
+	</div>
+	
+	  <form>		
+        <label>Name:</label>
+        <input name="name" placeholder="Type Here">
+		<label>Email:</label>
+		<input name="email" type="email" placeholder="Type Here">
+		<label>Message:</label>
+		<textarea name="message" placeholder="Type Here"></textarea>
+		<input id="submit" name="submit" type="submit" value="Submit">
+      </form>
+	
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html>
+  
+  <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+  
+  <head>
+    <title>
+	  The Googolplex Web blog
+    </title>
+	<script>
+	
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-100432608-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  </head>
+  
+  <body>
+	<div id="header">
+	  <a href="index.php">
+        <div class="navbox">Home</div>
+	  </a>
+	  <a href="downloads.php">
+	    <div class="navbox">Downloads</div>
+	  </a>
+	  <a href="recent.php"> 
+	    <div class="navbox">Recent Blogs</div>
+	  </a>
+	  <a href="about.php">
+	    <div class="navbox">About</div>
+	  </a>
+	  <a href="help.php">
+	    <div class="navbox" id="finalnav">Help / Contact</div>
+	  </a>
+	</div>
+	
+	<div id="greeting">
+	  <?php
+	  $beginning = fopen("welcome.txt", "r");
+	  while(!feof($beginning)) { 
+	  echo fgets($beginning) . "<br />";
+	  }
+	  fclose($beginning); 
+	  ?>
+	</div>
+	
+	<div id="recent1">
+  	  <?php 
+	
+	  ?>
+	</div>
+
+  </body>
+</html>
